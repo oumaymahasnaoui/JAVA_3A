a0c2d2f66614c10a47d64465eefc60146c08e764
oumaymahasnaoui <oumayma.hasnaoui@esprit.tn>
2023-09-23 14:29:13 +0100

    PROSIT_1

---
+++ Zoo.java.java
@@ -0,0 +1,10 @@
+public class ZooManagement {
+    public static void main(String[] args) {
+        // Déclaration des variables
+        int nbrCages = 20;
+        String zooName = "my zoo";
+
+        // Affichage du message
+        System.out.println(zooName + " comporte " + nbrCages + " cages.");
+    }
+}
